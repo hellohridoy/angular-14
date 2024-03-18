1a8459214c21fb96015febcd74c474713d556136
ridoy.hossain <ridoy.hossain@bracits.com>
2024-03-18 13:34:06 +0600

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './heroes-detail/heroes-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
--- src/app/heroes-detail/heroes-detail.component.ts
+++ src/app/heroes-detail/heroes-detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
-
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './heroes-detail.component.html',
   styleUrls: ['./heroes-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  constructor() {}
 
   ngOnInit(): void {}
+
+  @Input() hero?: Hero;
+  goBack() {}
 }
